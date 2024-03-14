482526a1c6057c689d68a63a1f39fc546bc098b4
TechnoGoku <bulatikilya@gmail.com>
2024-03-14 22:55:17 +0200

    update_2

--- home_work_task_1/home_work_4_task_1.py
+++ home_work_task_1/home_work_4_task_1.py
@@ -1,11 +1,12 @@
 from pathlib import Path
 
 def total_salary(path):
-    path = Path('User/Desktop/home_work_4/home_work_task_1/document.txt')
     with open(path, 'r', encoding='utf-8') as document:
-        read_file = document.read()
-        print(read_file)
+        document_file = document.read()
+        document_info = document_file.split(',')
+        document_info = []
+        print(document_info)
 
 
 
-total_salary('C:/Users/User/Desktop/home_work_4/home_work_task_1/document.txt') 
+path = total_salary('C:/Users/User/Desktop/home_work_4/home_work_task_1/document.txt') 
