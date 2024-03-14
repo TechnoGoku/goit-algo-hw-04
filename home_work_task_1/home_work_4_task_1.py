f5525468bd3c422110391e8e3fee0d73a70c6a17
TechnoGoku <bulatikilya@gmail.com>
2024-03-14 23:32:36 +0200

    update_5

--- home_work_task_1/home_work_4_task_1.py
+++ home_work_task_1/home_work_4_task_1.py
@@ -1,12 +1,19 @@
 from pathlib import Path
+import re
+import math
 
 def total_salary(path):
-    with open(path, 'r', encoding='utf-8') as document:
-        document_file = document.read()
-        document_info = document_file.splitlines()
-        document_info = [doc.strip() for doc in document_info if doc.strip()]
-        print(document_info)
-
+    try:
+        with open(path, 'r', encoding='utf-8') as document:
+            document_file = document.read()
+            # pattern = r"\d+"
+            numbers = re.findall(r"\d+", document_file)
+            document_info = document_file.splitlines()
+            document_info = [doc.strip() for doc in document_info if doc.strip()]
+            print(document_info)
+            print(numbers)
+    except:
+        pass
 
 
 path = total_salary('C:/Users/User/Desktop/home_work_4/home_work_task_1/document.txt') 
