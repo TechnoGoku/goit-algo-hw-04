c376291809c2ec1665bd38aa4b1f00397003568d
TechnoGoku <bulatikilya@gmail.com>
2024-03-15 00:01:39 +0200

    update_4

--- home_work_task_1/home_work_4_task_1.py
+++ home_work_task_1/home_work_4_task_1.py
@@ -1,21 +1,29 @@
 from pathlib import Path
 import re
-import math
 
 def total_salary(path):
     try:
         with open(path, 'r', encoding='utf-8') as document:
             document_file = document.read()
-            # pattern = r"\d+"
-            numbers = re.findall(r"\d+", document_file)
+            numbers_str = re.findall(r"\d+", document_file)
             document_info = document_file.splitlines()
             document_info = [doc.strip() for doc in document_info if doc.strip()]
-            print(document_info)
-            print(numbers)
+            numbers = []
+            for num_str in numbers_str:
+                numbers.append(int(num_str))
+            total = sum(numbers)
+            average = total // len(numbers)
+            print(f"Список працівників: {document_info}")
+            print(f"Заробітня плата працівників: {numbers}")
+            return total, average         
+            
     except:
         pass
 
 
-path = total_salary('C:/Users/User/Desktop/home_work_4/home_work_task_1/document.txt') 
+total, average = total_salary('C:/Users/User/Desktop/home_work_4/home_work_task_1/document.txt') 
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+
+
 
 
