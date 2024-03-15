3ff038b51153494c1689b18a2b09286a2126a01f
TechnoGoku <bulatikilya@gmail.com>
2024-03-15 10:35:59 +0200

    update_8

--- home_work_task_2/home_work_4_task_2.py
+++ home_work_task_2/home_work_4_task_2.py
@@ -1,9 +1,16 @@
 from pathlib import Path
 
 def get_cats_info(path):
+    pets_list = []
     with open(path, 'r', encoding='utf-8') as pets:
         pets_file = pets.read()
+        lines_n = pets_file.split('\n')
+        pets_list_info = []
+        for line in lines_n:
+            lines = line.split(',')
+            pets_list_info.extend(lines)
         print(pets_file)
+        print(pets_list_info)
 
 
 
