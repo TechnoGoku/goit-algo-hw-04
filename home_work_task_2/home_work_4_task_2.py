21e044ae4f31ce99ec8b85e13ab0d663060f1d99
TechnoGoku <bulatikilya@gmail.com>
2024-03-15 09:58:07 +0200

    update_7

--- home_work_task_2/home_work_4_task_2.py
+++ home_work_task_2/home_work_4_task_2.py
@@ -1,4 +1,11 @@
 from pathlib import Path
 
 def get_cats_info(path):
-    pass
+    with open(path, 'r', encoding='utf-8') as pets:
+        pets_file = pets.read()
+        print(pets_file)
+
+
+
+cats_info = get_cats_info("C:/Users/User/Desktop/home_work_4/home_work_task_2/pets.txt")
+# print(cats_info)
